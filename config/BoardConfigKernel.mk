3c1ad0f59b7a66a507531156ca0624a0f521aafb
Panchajanya Sarkar <panchajanya@azure-dev.live>
2020-04-23 18:11:27 +0000

    BoardConfigKernel: Allow devices to use newer clang
    Allows kernel to be compiled with newer clang 11 toolchains. Ensure that
    the toolchain must be cloned manually in prebuilts/clang/host/linux-x86/clang-11
    
    Toolchain Used : https://github.com/Panchajanya1999/azure-clang.git
    
    Signed-off-by: Panchajanya Sarkar <panchajanya@azure-dev.live>

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -27,6 +27,10 @@
 #                                                      aarch64-linux-android- for arm64
 #
 #   TARGET_KERNEL_CLANG_COMPILE        = Compile kernel with clang, defaults to false
+#
+#   TARGET_KERNEL_NEW_CLANG_COMPILE    = Compile kernel with clang-11, defaults to false
+#										  Use this with TARGET_KERNEL_CLANG_COMPILE enabled
+#
 #   TARGET_KERNEL_NEW_GCC_COMPILE      = Compile kernel with newer version GCC, defaults to false
 #
 #   KERNEL_TOOLCHAIN_PREFIX            = Overrides TARGET_KERNEL_CROSS_COMPILE_PREFIX,
@@ -137,10 +141,17 @@ ifneq ($(TARGET_KERNEL_ADDITIONAL_FLAGS),)
   KERNEL_MAKE_FLAGS += $(TARGET_KERNEL_ADDITIONAL_FLAGS)
 endif
 
-TOOLS_PATH_OVERRIDE := \
+ifeq ($(TARGET_KERNEL_NEW_CLANG_COMPILE),true)
+TOOLS_PATH_OVERRIDE += \
+    PATH=$(BUILD_TOP)/prebuilts/clang/host/linux-x86/clang-11/bin:$(BUILD_TOP)/prebuilts/tools-extras/$(HOST_OS)-x86/bin:$$PATH \
+    LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/clang/host/linux-x86/clang-11/lib:$(BUILD_TOP)/prebuilts/tools-extras/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-extras/common/perl-base
+else
+TOOLS_PATH_OVERRIDE += \
     PATH=$(BUILD_TOP)/prebuilts/tools-extras/$(HOST_OS)-x86/bin:$$PATH \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-extras/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
     PERL5LIB=$(BUILD_TOP)/prebuilts/tools-extras/common/perl-base
+endif
 
 # Set DTBO image locations so the build system knows to build them
 ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)
